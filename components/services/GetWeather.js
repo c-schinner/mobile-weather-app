4b47687024d49346dac3cb5fa40f68151c69d482
Chris <cschinner7@gmail.com>
2024-09-30 10:30:13 -0700

    fixed the env file with my API key

--- components/services/GetWeather.js
+++ components/services/GetWeather.js
@@ -1,18 +1,20 @@
-const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
+import { WEATHER_API_KEY } from '@env';
+
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
 
 export const iconUrlFromCode = (icon) => `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
 export const fetchWeatherData = async (infoType, searchParams) => {
     const url = new URL(BASE_URL + infoType);
-    const newSearchParams = {
-        units: searchParams.units,
-        q: searchParams.q,
-        appid: API_KEY,
-    };
 
-    console.log(newSearchParams);
-    url.search = new URLSearchParams(newSearchParams);
+    console.log("Using API Key:", WEATHER_API_KEY);
+    console.log("Search PArams:", searchParams);
+
+    url.searchParams.append('units', searchParams.units);
+    url.searchParams.append('q', searchParams.q);
+    url.searchParams.append('appid', WEATHER_API_KEY);
+
+    console.log("Final Request URL:", url.toString());
 
     const res = await fetch(url);
     return await res.json();
